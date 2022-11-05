dc5edbba999832117df5ee4e6662b2cbeddbb0ac
Henry Ohia <henryohia197@gmail.com>
2022-11-05 23:28:37 +0100

    Added Greed game

---
+++ greed/game/casting/actor.py
@@ -0,0 +1,143 @@
+from game.shared.color import Color
+from game.shared.point import Point
+
+
+class Actor:
+    """A visible, moveable thing that participates in the game. 
+    
+    The responsibility of Actor is to keep track of its appearance, position and velocity in 2d 
+    space.
+
+    Attributes:
+        _text (string): The text to display
+        _font_size (int): The font size to use.
+        _color (Color): The color of the text.
+        _position (Point): The screen coordinates.
+        _velocity (Point): The speed and direction.
+
+    """
+
+    def __init__(self):
+        """Constructs a new Actor."""
+        self._text = ""
+        self._font_size = 15
+        self._color = Color(255, 255, 255)
+        self._position = Point(0, 0)
+        self._velocity = Point(0, 0)
+
+    def get_color(self):
+        """Gets the actor's color as a tuple of three ints (r, g, b).
+        
+        Returns:
+            Color: The actor's text color.
+        """
+        return self._color
+
+    def get_font_size(self):
+        """Gets the actor's font size.
+        
+        Returns:
+            Point: The actor's font size.
+        """
+        return self._font_size
+
+    def get_position(self):
+        """Gets the actor's position in 2d space.
+        
+        Returns:
+            Point: The actor's position in 2d space.
+        """
+        return self._position
+    
+    def get_text(self):
+        """Gets the actor's textual representation.
+        
+        Returns:
+            string: The actor's textual representation.
+        """
+        return self._text
+
+    def get_velocity(self):
+        """Gets the actor's speed and direction.
+        
+        Returns:
+            Point: The actor's speed and direction.
+        """
+        return self._velocity
+    
+    def move_next(self, height, max_x, max_y):
+        """Moves the actor to its next position according to its velocity. Will wrap the position 
+        from one side of the screen to the other when it reaches the given maximum x value.
+        
+        Args:
+            height (int): The height of the subarea where to play.
+            max_x (int): The maximum x value.
+            max_y (int): The maximum y value.
+        """
+        up_limit = max_y - height
+        x = (self._position.get_x() + self._velocity.get_x()) % max_x
+        y = self._position.get_y() + self._velocity.get_y()
+        if y < up_limit:
+            y = self._position.get_y()
+        elif y >= max_y:
+            y = self._position.get_y()
+        
+        self._position = Point(x, y)
+
+    def set_color(self, color):
+        """Updates the color to the given one.
+        
+        Args:
+            color (Color): The given color.
+        """
+        self._color = color
+
+    def set_position(self, position):
+        """Updates the position to the given one.
+        
+        Args:
+            position (Point): The given position.
+        """
+        self._position = position
+    
+    def set_font_size(self, font_size):
+        """Updates the font size to the given one.
+        
+        Args:
+            font_size (int): The given font size.
+        """
+        self._font_size = font_size
+    
+    def set_text(self, text):
+        """Updates the text to the given value.
+        
+        Args:
+            text (string): The given value.
+        """
+        self._text = text
+
+    def set_velocity(self, velocity):
+        """Updates the velocity to the given one.
+        
+        Args:
+            velocity (Point): The given velocity.
+        """
+        self._velocity = velocity
+'''
+    # Example how to create the player at the bottom of the window
+    max_x = 900
+    max_y = 600
+    cell_size = 20
+    font_size = 20
+    cols = int(max_x / cell_size)
+    rows = int(max_y / cell_size)
+    x = int(cols / 2)
+    y = rows - 1
+    position = Point(x, y).scale(cell_size)
+
+    player = Actor()
+    player.set_text("#")
+    player.set_font_size(font_size)
+    player.set_color(Color(255,255,255))
+    player.set_position(position)
+'''
---
+++ greed/game/directing/director.py
@@ -0,0 +1,96 @@
+#cole & antonio
+import time
+
+class Director:
+    """This class directs the flow of the program. It will call various classes for functionality.
+
+    Attributes:
+        keyboard_service: for getting directional input.
+        video_service: for providing video output.
+        points: (int) keeps track of points."""
+
+    def __init__(self, keyboard_service, video_service, CELL_SIZE, HEIGHT_PLAYING_AREA):
+        """Constructs a new Director using the specified keyboard and video services. Initializing points for player. 
+
+        Args:
+            keyboard_service: instance of keyboard service.
+            video_service: instance of video service.
+            CELL_SIZE (integer): the size of the cells.
+            HEIGHT_PLAYING_AREA (integer): the height of the playing area for the player.
+        """
+        self._keyboard_service = keyboard_service
+        self._video_service = video_service
+        self._points = 100
+        self._action_message = ""
+        self._CELL_SIZE = CELL_SIZE
+        self._HEIGHT_PLAYING_AREA = HEIGHT_PLAYING_AREA
+
+    def start_game(self, cast):
+        """Starts the game using the given cast.
+
+        Args:
+            cast: the cast of actors"""
+        self._video_service.open_window()
+        while self._video_service.is_window_open():
+            self._get_inputs(cast)
+            self._do_updates(cast)
+            self._do_outputs(cast)
+            if self._points <= 0:
+                time.sleep(2)
+                self._video_service.close_window()
+        self._video_service.close_window()
+
+    def _get_inputs(self, cast):
+        """Gets directional input from the keyboard and applies it to the player.
+
+        Args:
+            cast: the cast of actors."""
+        player = cast.get_first_actor("player")
+        velocity = self._keyboard_service.get_direction()
+        player.set_velocity(velocity)
+
+    def _do_updates(self, cast):
+        """Updates the players position and resolves any collisions with rocks and gems.
+
+        Args:
+            cast: the cast of actors."""
+        banner = cast.get_first_actor("banners")
+        action = cast.get_first_actor("action")
+        player = cast.get_first_actor("player")
+        gems = cast.get_actors("gems")
+        rocks = cast.get_actors("rocks")
+
+        banner.set_text(f"Score {self._points}")
+        action.set_text(self._action_message)
+        max_x = self._video_service.get_width()
+        max_y = self._video_service.get_height()
+        player.move_next(self._HEIGHT_PLAYING_AREA, max_x, max_y)
+
+        for gem in gems:
+            gem.move_down()
+            if player.get_position().equals(gem.get_position()):
+                self._action_message = gem.get_message()
+                self._points += gem.get_value()
+                gem.set_random_position(self._CELL_SIZE, max_x, max_y)
+            elif gem.get_position().get_y() >= max_y:
+                gem.set_random_position(self._CELL_SIZE, max_x, max_y)
+        for rock in rocks:
+            rock.move_down()
+            if player.get_position().equals(rock.get_position()):
+                self._action_message = rock.get_message()
+                self._points -= rock.get_value()
+                rock.set_random_position(self._CELL_SIZE, max_x, max_y)
+            elif rock.get_position().get_y() >= max_y:
+                rock.set_random_position(self._CELL_SIZE, max_x, max_y)
+        if self._points <= 0:
+            banner.set_text("Game Over")
+
+    def _do_outputs(self, cast):
+        """Draws the actors on the screen.
+
+        Args:
+            cast: the cast of actors."""
+        self._video_service.clear_buffer()
+        actors = cast.get_all_actors()
+        self._video_service.draw_actors(actors)
+        self._video_service.flush_buffer()
